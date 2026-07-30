36eec0e80a33832021888eb9bfa76c7012b247ec
saxani <m.syazani@sutera.biz>
2026-07-30 22:35:32 +0800

    created: variables.tf

--- terraform2/network.tf
+++ terraform2/network.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.20.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = var.az
   map_public_ip_on_launch = true
   tags = {
     Name = "tf-subnet-public"
